c1ab07511a85c21664ea236f555455c35a9b378e
Brek <brek.graham@gmail.com>
2024-08-06 14:08:36 -0500

    Changed: ALTER TABLE mission_point ADD COLUMN iso_timestamp TEXT; To ALTER TABLE mission_point ADD COLUMN creation_timestamp_iso TEXT;

--- sql/add_creation_columns.sql
+++ sql/add_creation_columns.sql
@@ -3,5 +3,5 @@ ALTER TABLE mission_point ADD COLUMN creation_month INTEGER;
 ALTER TABLE mission_point ADD COLUMN creation_day INTEGER;
 ALTER TABLE mission_point ADD COLUMN creation_time TEXT;
 ALTER TABLE mission_point ADD COLUMN creation_timestamp TEXT;
-ALTER TABLE mission_point ADD COLUMN iso_timestamp TEXT;
+ALTER TABLE mission_point ADD COLUMN creation_timestamp_iso TEXT;
 UPDATE mission_point SET creation_timestamp = CreationDate;
