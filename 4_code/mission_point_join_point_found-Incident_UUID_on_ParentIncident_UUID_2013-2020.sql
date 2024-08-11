f219da06c46a0b80a5494a59eb620e9f37a5d3f3
Brek <brek.graham@gmail.com>
2024-08-11 14:26:15 -0500

    updated column naming

--- 4_code/mission_point_join_point_found-Incident_UUID_on_ParentIncident_UUID_2013-2020.sql
+++ 4_code/mission_point_join_point_found-Incident_UUID_on_ParentIncident_UUID_2013-2020.sql
@@ -1,15 +1,15 @@
 -- Join mission points to point found on Incident_UUID = Linked_Incident_UUID
 SELECT 
-  mra.Incident_UUID,
+  mp.Incident_UUID,
   pf.Linked_Incident_UUID,
-  mra.IncidentNumber,
-  mra.IncidentStartDateandTime,
-  mra.TeamName,
-  mra.CreationDate,
-  mra.creation_timestamp_iso
+  mp.IncidentNumber,
+  mp.IncidentStartDateandTime,
+  mp.TeamName,
+  mp.CreationDate,
+  mp.creation_timestamp_iso
 FROM 
-  mission_point mra
+  mission_point mp
 LEFT JOIN 
-  point_found pf ON mra.Incident_UUID = pf.Linked_Incident_UUID
+  point_found pf ON mp.Incident_UUID = pf.Linked_Incident_UUID
 GROUP BY 
   creation_timestamp_iso
