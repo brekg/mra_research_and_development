dd96ee8c692727483752b59539b34f566dfe5610
Brek <brek.graham@gmail.com>
2024-08-11 14:21:19 -0500

    updated the naming

--- 4_code/mission_point_join_point_found-GlobalID_on_ParentGlobalID_2013-2020.sql
+++ 4_code/mission_point_join_point_found-GlobalID_on_ParentGlobalID_2013-2020.sql
@@ -1,15 +1,17 @@
 -- Join mission points to point found on mra.GlobalID = pf.ParentGlobalID
 SELECT 
-  mra.Incident_UUID,
+  mp.GlobalID,
+  pf.ParentGlobalID,
+  mp.Incident_UUID,
   pf.Linked_Incident_UUID,
-  mra.IncidentNumber,
-  mra.IncidentStartDateandTime,
-  mra.TeamName,
-  mra.CreationDate,
-  mra.creation_timestamp_iso
+  mp.IncidentNumber,
+  mp.IncidentStartDateandTime,
+  mp.TeamName,
+  mp.CreationDate,
+  mp.creation_timestamp_iso
 FROM 
-  mission_point mra
+  mission_point mp
 LEFT JOIN 
-  point_found pf ON mra.GlobalID = pf.ParentGlobalID
+  point_found pf ON mp.GlobalID = pf.ParentGlobalID
 GROUP BY 
   creation_timestamp_iso
